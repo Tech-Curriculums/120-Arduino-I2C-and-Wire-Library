c28e509af3bc18663d11e9e60afc0b34eb834bde
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-08-04 18:10:28 -0700

    Update NightLight.ino

--- Project_1/NightLight.ino
+++ Project_1/NightLight.ino
@@ -3,33 +3,37 @@
 void setup(){
   Serial.begin(9600);  
   Serial.println("Serial Connection Established");
-  pinMode(3, OUTPUT);
 }
 
 //0x23 is the bh1750's I2C Address as I'm running it in address low mode 
 //i.e. I am connecting "ADD" to "GND"
 void loop() {
-     
   Wire.beginTransmission(0x23);
   Wire.write(0x01); //power on
-  delay(120);
+  Wire.endTransmission();
   
-  Wire.requestFrom(0x23,2); //requestnig 2 bytes from the lux sensor
+  Wire.beginTransmission(0x23);
+  Wire.write(0x20); //one time sense
+  Wire.endTransmission();
+  delay(500);
+  
+  Wire.beginTransmission(0x23);
+  Wire.requestFrom(0x23,2); //requesting 2 bytes from the lux sensor
   
   
     //read them in one byte at a time
+    
+
      uint16_t lux = Wire.read();
-      
+       
     //MSB was first so we'll need to shift byte to the left 
      lux = lux << 8; //
 
      // "or"-ing with the next byte, we will be able to  
      lux |= Wire.read();
-      
-     if ( lux < 100 )
-       digitalWrite(3, HIGH);
-     else
-       digitalWrite(3, LOW); 
-      
-     Serial.println(lux);  
+ 
+   Wire.endTransmission();
+
+     Serial.println(lux);
+     
 }
